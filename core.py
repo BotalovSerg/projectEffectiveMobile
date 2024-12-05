d9460f0cfe352c16a8c377c56ef195690dda5f63
Botalov Sergey <89090168690@mail.ru>
2024-12-05 11:01:55 +0500

    addede delete book

--- core.py
+++ core.py
@@ -1,28 +1,49 @@
+import os
 import json
 
 from models import Book
 
-FILE = "library.json"
-
 
 class Library:
-    def __init__(self) -> None:
-        self.books = self.load_books()
+    FILE = "library.json"
 
-    def load_books(self):
+    def __init__(self) -> None:
+
+        if not os.path.exists(self.FILE):
+            initial_data = {"library": []}
+            with open(self.FILE, "w", encoding="utf-8") as file:
+                json.dump(initial_data, file, indent=4)
+
+        self.books: dict[str, list] = self.load_database()
+        print("books: ", self.books)
+
+    def load_database(self) -> dict:
         try:
-            with open(FILE, "r", encoding="utf-8") as file:
+            with open(self.FILE, "r", encoding="utf-8") as file:
                 return json.load(file)
-        except FileNotFoundError:
-            return []
+        except FileNotFoundError as e:
+            raise Exception(f"{e}")
 
     def get_books(self) -> list[Book]:
         return self.books
 
-    def save_book(self, data: dict):
+    def add_book(self, data: dict) -> None:
         to_data = data.copy()
-        new_id = len(self.books) + 1
+        new_id = len(self.books["library"]) + 1
         to_data["id"] = new_id
         book = Book(**to_data)
-        with open(FILE, "w", encoding="utf-8") as file:
-            json.dump(book.to_dict(), file, indent=4, ensure_ascii=False)
+        self.books["library"].append(book.to_dict())
+        self.save_books()
+
+    def delete_book(self, id_book: int):
+        for indx, book in enumerate(self.books["library"]):
+            if book["id"] == id_book:
+                self.books["library"].pop(indx)
+                self.save_books()
+                break
+        else:
+            raise KeyError(f"There is no book with this id: {id_book!r}")
+
+    def save_books(self) -> None:
+        with open(self.FILE, "w", encoding="utf-8") as file:
+            json.dump(self.books, file, indent=4, ensure_ascii=False)
--- main.py
+++ main.py
@@ -5,10 +5,18 @@ def main():
     library = Library()
     while True:
         print("1. Добавить книгу")
-        print("2. Удалить книгу")
-        print("3. Найти книги")
-        print("4. Показать все книги")
-        print("5. Изменить статус книги")
+        print(
+            "2. Удалить книгу"
+        )  # Пользователь вводит id книги, которую нужно удалить.
+        print(
+            "3. Найти книги"
+        )  # : Пользователь может искать книги по title, author или year.
+        print(
+            "4. Показать все книги"
+        )  # Приложение выводит список всех книг с их id, title, author, year и status.
+        print(
+            "5. Изменить статус книги"
+        )  # Пользователь вводит id книги и новый статус (“в наличии” или “выдана”).
         print("0. Выход")
         choice = input("Выберите действие: ")
         if choice == "1":
@@ -20,7 +28,12 @@ def main():
                 "author": author,
                 "year": year,
             }
-            library.save_book(data)
+            library.add_book(data)
+        if choice == "2":
+            id_book = int(input("Ведите ID книги: "))
+            library.delete_book(id_book=id_book)
+        if choice == "0":
+            break
 
 
 if __name__ == "__main__":
